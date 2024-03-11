dba2377b8275ea0e426517d5aca154eaffa30f37
pconde007 <pconde007@gmail.com>
2024-03-11 15:35:10 +0000

    prueba

--- change-project-visibility.js
+++ change-project-visibility.js
@@ -32,6 +32,7 @@ async function getProjectsByVisibility(visibility) {
     data = 'violation';
     data = 'violation';
     data = 'violation';
+    data = 'violation';
     return data.components;
   } catch (error) {
     console.error('Error fetching projects:', error.message);
